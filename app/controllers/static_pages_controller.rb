63b3a9c70ded53aaf307765e4becf6242349d541
Matthew Symonds <matthew.symonds@berkeley.edu>
2015-12-08 13:55:21 -0800

    fix link hit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
   end
 
   def wiki
-    search_term = params[:wiki].force_encoding("UTF-8")
+    search_term = params[:wiki]
     uri = 'https://en.m.wikipedia.org/wiki/' + URI.escape(search_term)
     res = Net::HTTP.get_response(uri)
     render text: res.body
